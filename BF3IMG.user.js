82cd693cd70a66bbfe8a8434488e9784e85443c0
u5545 <39065459+u5545@users.noreply.github.com>
2018-11-30 07:32:39 +0100

    Update BF3IMG.user.js

--- BF3IMG.user.js
+++ BF3IMG.user.js
@@ -2,16 +2,17 @@
 // @name         BF3IMG
 // @namespace    https://github.com/u5545/
 // @author       u5545
-// @version      1.0
 // @description  beastforum free images
 // @match        https://www.beastforum.com/*
 // @downloadURL  https://raw.githubusercontent.com/u5545/userscripts/master/BF3IMG.user.js
 // @updateURL    https://raw.githubusercontent.com/u5545/userscripts/master/BF3IMG.user.js
+// @version      1.0
 // @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
-    document.querySelectorAll("img.attach").forEach(function(element){
-        element.parentElement.setAttribute("href", element.getAttribute("src").replace(/(.*\/)\w{5}(post.*)/, "$1$2"));
+    document.querySelectorAll("img.attach").forEach(function(e){
+        e.parentElement.setAttribute("href", e.getAttribute("src").replace(/(.*\/)\w{5}(post.*)/, "$1$2"));
     });
 })();
